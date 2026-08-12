d6bd05552e4930f3d2511e21d74c0a33b3af43f7
mishkacher <73113517+mishkacher@users.noreply.github.com>
2026-08-12 21:33:18 +0700

    Load five-block reference layout

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './styles.css'
 import './mobile-compact.css'
 import './source-labels.css'
+import './reference-layout.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
