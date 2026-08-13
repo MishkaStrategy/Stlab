13ddc56e5f9be748341012880fdc686468b6ae1a
mishkacher <73113517+mishkacher@users.noreply.github.com>
2026-08-13 11:44:46 +0700

    Add works concept entry

---
+++ src/works-concept.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import WorksConceptPage from './WorksConceptPage'
+import './works-concept.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <WorksConceptPage />
+  </React.StrictMode>,
+)
